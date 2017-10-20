2ec74520f29059057ef130c141599fc3ee5a8417
Andrew Stead <andrew@andrewstead.co.uk>
2017-10-20 08:22:53 +0100

    Edited lightbox.js to restrict gallery to active month

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,22 +1,21 @@
+/* Set variable to control activeImage element. */
+var lightbox = document.getElementById("activeImage");
+
 /* Set variable for default HTML of activeImage element. */
 var boxControls = document.getElementById("activeImage").innerHTML;
 
-function openBox(imagePosition) {
-	/* When an image is clicked, give the activeImage element a grey background and set a class for large image display. */
-	var lightbox = document.getElementById("activeImage");
-	lightbox.className += " lightsDown fullImage";
+function openBox(month, imagePosition) {
+	/* When an image is clicked, display the activeImage element. */
 	lightbox.style.display = "block";
 
 	/* Select the correct image from the walkGallery array and put its HTML in the activeImage element. */
-	var walkGallery = document.getElementsByClassName("lightbox");
+	var walkGallery = document.getElementsByClassName(month);
 	var chosenImage = walkGallery[imagePosition-1];
 	lightbox.innerHTML += chosenImage.innerHTML;
 }
 
 function closeBox() {
-	/* When the lightbox is clicked, remove the lightbox classes and the HTML from the activeImage element. */
-	var lightbox = document.getElementById("activeImage");
+	/* When the close icon is clicked, remove the image HTML from the activeImage element and hide it. */
 	lightbox.style.display = "none";
 	lightbox.innerHTML = boxControls;
-	lightbox.className -= "lightsDown fullImage";
 }
