65a83d33effbce42f4474adecbc43b59a69d2ef6
Andrew Stead <andrew@andrewstead.co.uk>
2017-10-20 11:13:44 +0100

    Edited lightbox.js to allow limited navigation between images

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,21 +1,38 @@
 /* Set variable to control activeImage element. */
 var lightbox = document.getElementById("activeImage");
 
-/* Set variable for default HTML of activeImage element. */
-var boxControls = document.getElementById("activeImage").innerHTML;
+var chosenImage;
+var activeMonth;
+
+var boxControls = '\
+	<div id="boxCloser" onclick="closeBox()">&times;</div>\
+	<div id="previousImage" onclick="previousImage()">&lt;</div>\
+	<div id="nextImage" onclick="nextImage()">&gt;</div>\
+	';
 
 function openBox(month, imagePosition) {
 	/* When an image is clicked, display the activeImage element. */
 	lightbox.style.display = "block";
 
 	/* Select the correct image from the walkGallery array and put its HTML in the activeImage element. */
-	var walkGallery = document.getElementsByClassName(month);
-	var chosenImage = walkGallery[imagePosition-1];
-	lightbox.innerHTML += chosenImage.innerHTML;
+	chosenImage = imagePosition;
+	activeMonth = month;
+	lightbox.innerHTML = boxControls + '<img src="img/' + month + '/' + imagePosition +'.jpg" alt="Walk Image" />';
 }
 
+/* After an image is selected, the user can move back to the previous image. */
+function previousImage() {
+	var newImage = chosenImage--;
+	lightbox.innerHTML = boxControls + '<img src="img/' + activeMonth + '/' + newImage +'.jpg" alt="Walk Image" />';
+}
+
+/* After an image is selected, the user can move on to the next image. */
+function nextImage() { 
+	var newImage = chosenImage++;
+	lightbox.innerHTML = boxControls + '<img src="img/' + activeMonth + '/' + newImage +'.jpg" alt="Walk Image" />';
+}
+
+/* When the close icon is clicked, remove the image HTML from the activeImage element and hide it. */
 function closeBox() {
-	/* When the close icon is clicked, remove the image HTML from the activeImage element and hide it. */
 	lightbox.style.display = "none";
-	lightbox.innerHTML = boxControls;
 }
