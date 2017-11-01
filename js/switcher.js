b90ead76f0448483b0706f0a8ec831aff1c144b3
Andrew Stead <andrew@andrewstead.co.uk>
2017-11-01 00:32:08 +0000

    Revised switcher.js file to improve code while retaining functionality

--- js/switcher.js
+++ js/switcher.js
@@ -1,20 +1,26 @@
-/* Run default display function when page loads. */
-window.onload = defaultDisplay();
-
-/* Display the first gallery in the list and hide all others on page load.  */
-function defaultDisplay() {
-	var walkList = document.getElementsByClassName("walkSwitcher");
-	for (var i = 0; i < walkList.length; i++) {
-		walkList[i].style.display = "none";
-	}
-	walkList[0].style.display = "block";
-}
-
-/* Display a given gallery and hide all others when the relevant link is clicked.  */
+/* Function to display a particular photo gallery. */
 function contentSwitcher(month) {
+
+	/* Variable to create array of elements with walkSwitcher class. */
 	var walkList = document.getElementsByClassName("walkSwitcher");
-	for (var i = 0; i < walkList.length; i++) {
-		walkList[i].style.display = "none";
+
+	/* If no month has been selected, display the first gallery by adding the activeGallery class. */
+	if (month === undefined) {
+		walkList[0].classList.add("activeGallery");
+
+	/* Display the relevant gallery when a month is selected. */
+	} else {
+
+		/* Hide visible gallery by removing the activeGallery class. */
+		for (var i = 0; i < walkList.length; i++) {
+			walkList[i].classList.remove("activeGallery");
+		}
+
+		/* Display the chosen gallery by adding the activeGallery class. */
+		document.getElementById(month).classList.add("activeGallery");
 	}
-	document.getElementById(month).style.display = "block";
+
 }
+
+/* Run function when page loads in order to display default gallery. */
+contentSwitcher();
