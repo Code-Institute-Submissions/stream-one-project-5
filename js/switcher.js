7c72ab17dcfee9f9f98a53e8a56d06d598b4af1d
Andrew Stead <andrew@andrewstead.co.uk>
2017-11-07 12:48:20 +0000

    Edited switcher.js to work with Photo Galleries

--- js/switcher.js
+++ js/switcher.js
@@ -4,18 +4,18 @@ function contentSwitcher(month) {
 	/* Variable to create array of elements with walkSwitcher class. */
 	var walkList = document.getElementsByClassName("walkSwitcher");
 
-	/* If no month has been selected, display the first gallery by adding the activeGallery class. */
+	/* If no month has been selected, display the first gallery by adding the activeMonth class. */
 	if (month === undefined) {
-		walkList[0].classList.add("activeGallery");
+		walkList[0].classList.add("activeMonth");
 
 	/* Display the relevant gallery when a month is selected. */
 	} else {
-		/* Ensure the previous gallery is hidden by removing the activeGallery class. */
+		/* Ensure the previous gallery is hidden by removing the activeMonth class. */
 		for (var i = 0; i < walkList.length; i++) {
-			walkList[i].classList.remove("activeGallery");
+			walkList[i].classList.remove("activeMonth");
 		}
-		/* Display the chosen gallery by adding the activeGallery class. */
-		document.getElementById(month).classList.add("activeGallery");
+		/* Display the chosen gallery by adding the activeMonth class. */
+		document.getElementById(month).classList.add("activeMonth");
 	}
 
 }
